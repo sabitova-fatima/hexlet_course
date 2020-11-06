70778df7a71d0076efafa712c6ac2ca5d3937b5a
Fatima <sabitova.fatima@gmail.com>
2020-11-06 22:46:31 +0300

    Update ifelse.c

--- ifelse.c
+++ ifelse.c
@@ -7,21 +7,20 @@ int power (int, int);
 int main()
 {
 
-printf("%d\n", power(3,4));
-printf("i am doing nothing\n");
-return 0;
+  printf("%d\n", power(3,4));
+  printf("i am doing nothing\n");
+  return 0;
 
-// this is a comment
+  // this is a comment
 
-/*
-
-this is a long comment
-and it has two lines
-
-*/
+  /*
+  this is a long comment
+  and it has two lines
+  */
+}
 
 int power(int base, int n)
-{
+  {
 
   int i, p;
   p = 1;
@@ -35,8 +34,4 @@ int power(int base, int n)
   for(i = 1; i <= n; ++i)
     p = p*base;
     return 0;
-  }
-}
-
-
-}
+ }
