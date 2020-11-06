14415867937b5486f8d873b9cb3bce120c51d0ba
Fatima <sabitova.fatima@gmail.com>
2020-11-06 22:44:11 +0300

    Update massives.c

--- massives.c
+++ massives.c
@@ -3,21 +3,20 @@
 int main()
 {
 
-int arr[4];
+  int arr[4];
+  int i;
+  int sum;
+  
+  arr[0] = 32;
+  arr[1] = 12;
+  arr[2] = 42;
+  arr[3] = 99;
 
-arr[0] = 32;
-arr[1] = 12;
-arr[2] = 42;
-arr[3] = 99;
+  sum = 0;
 
-int i, sum;
-sum = 0;
-
-for(i=0; i<4; ++i)
-sum = sum + arr[i];
-
-printf("%d\n", sum);
-
-return 0;
+  for (i = 0; i < 4; ++i)
+  sum = sum + arr[i];
+  printf("%d\n", sum);
+  return 0;
 }
 
